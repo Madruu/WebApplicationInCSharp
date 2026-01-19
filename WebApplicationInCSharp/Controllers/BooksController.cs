31c5c6015a8cf33b6d68fa3029d96083ee8181c7
Vitor Tulio <vitordtulio@gmail.com>
2026-01-19 13:36:05 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplicationInCSharp/Controllers/BooksController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
+using WebApplicationInCSharp.Models;
+namespace WebApplicationInCSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var books = _context.Book.ToList();
+
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBook([FromBody] Book book)
+        {
+            _context.Book.Add(book);
+            await _context.SaveChangesAsync();
+
+            return Ok(book);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, [FromBody] Book book)
+        {
+            var rows = await _context.Book
+                .Where(b => b.ID == id)
+                .ExecuteUpdateAsync(b => b
+                    .SetProperty(bk => bk.Title, book.Title)
+                    .SetProperty(bk => bk.Author, book.Author)
+                    .SetProperty(bk => bk.YearPublished, book.YearPublished)
+                );
+
+
+            return Ok(book);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var rows = await _context.Book.Where(b => b.ID == id).ExecuteDeleteAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ WebApplicationInCSharp/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplicationInCSharp.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = null!; //Not gonna be null at runtime
+        public string Author { get; set; } = null!; //Not gonna be null at runtime
+        public int YearPublished { get; set; }
+    }
+}
